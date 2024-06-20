eb2612a0cbab4a7bc25cc568d0d8da8e590088d1
Msyauqi <muhammad.535220221@stu.untar.ac.id>
2024-06-20 23:17:17 +0700

    membuat table view_categories terhubung ke PgAdmin

--- resources/views/admin_area/view_categories.blade.php
+++ resources/views/admin_area/view_categories.blade.php
@@ -1,19 +1,21 @@
 <table class="table">
-    <thead class="thead-dark">
-      <tr>
-        <th scope="col">id</th>
-        <th scope="col">Title_Categories</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-
-      </tr>
-    </tbody>
-  </table>
-  
-  
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">Title_Categories</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($categories ?? [] as $category)
+    <tr>
+      <th scope="row">{{ $category->id }}</th>
+      <td>{{ $category->categories_title }}</td>
+      <td>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editCategoryModal" data-bs-id="{{ $category->id }}" data-bs-title="{{ $category->categories_title }}">Edit</button>
+        <a href="{{ url('admin_area/delete_category', $category->id) }}" class="btn btn-danger">Delete</a>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
