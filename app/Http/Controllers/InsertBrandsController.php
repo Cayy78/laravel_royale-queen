ea14ceac45d5e3261e6ca131f4b77b8cc4fe8600
Msyauqi <muhammad.535220221@stu.untar.ac.id>
2024-06-20 12:12:56 +0700

    membuat table data Categories

---
+++ app/Http/Controllers/InsertBrandsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InsertBrands;
+use Illuminate\Http\Request;
+
+class InsertBrandsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\InsertBrands  $insertBrands
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InsertBrands $insertBrands)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\InsertBrands  $insertBrands
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InsertBrands $insertBrands)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\InsertBrands  $insertBrands
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InsertBrands $insertBrands)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InsertBrands  $insertBrands
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InsertBrands $insertBrands)
+    {
+        //
+    }
+}
---
+++ resources/views/admin_area/view_categories.blade.php
@@ -0,0 +1,19 @@
+<table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">Title_Categories</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+
+      </tr>
+    </tbody>
+  </table>
+  
+  
